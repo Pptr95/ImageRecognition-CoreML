a457748e0ce74b61086b691976da644fd98ac40a
Petru Potrimba <petru.potrimba@studio.unibo.it>
2018-07-29 21:44:07 +0200

    create ComputerVision project and create user interface

---
+++ ComputerVision/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  ComputerVision
+//
+//  Created by Valerio Potrimba on 29/07/2018.
+//  Copyright © 2018 Petru Potrimba. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var textArea: UITextView!
+    @IBOutlet weak var picture: UIImageView!
+    @IBAction func selectPhoto(_ sender: UIButton) {
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
