52abbb0e00cb217e1b5703988700a5df65bded56
Petru Potrimba <petru.potrimba@studio.unibo.it>
2018-07-29 19:03:52 +0200

    add recognizer image feature using core ML

--- ImageRecognitionCoreML/ViewController.swift
+++ ImageRecognitionCoreML/ViewController.swift
@@ -13,13 +13,18 @@ class ViewController: UIViewController {
     @IBOutlet weak var recognizeLabel: UILabel!
     @IBOutlet weak var imageView: UIImageView!
     @IBAction func next(_ sender: UIButton) {
-        let img = UIImage(named: self.images[self.current])?.resizeTo(size: CGSize(width: 299, height: 299))
+        let img = UIImage(named: self.images[self.current])
         self.imageView.image = img
         self.current = (self.current + 1) % self.images.count
+        let resizedImage = img?.resizeTo(size: CGSize(width: 299, height: 299))
+        let bufferedImage = resizedImage?.toBuffer()
+        let prediction = try! self.model.prediction(image: bufferedImage!)
+        self.recognizeLabel.text = prediction.classLabel
     }
     
     let images = ["dog.jpg", "cat.jpg", "rat.jpg", "banana.jpg"]
     var current: Int = 0
+    private var model: Inceptionv3 = Inceptionv3()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -38,4 +43,31 @@ extension UIImage {
         return resizedImage
         
     }
+    
+    func toBuffer() -> CVPixelBuffer? {
+        
+        let attrs = [kCVPixelBufferCGImageCompatibilityKey: kCFBooleanTrue, kCVPixelBufferCGBitmapContextCompatibilityKey: kCFBooleanTrue] as CFDictionary
+        var pixelBuffer : CVPixelBuffer?
+        let status = CVPixelBufferCreate(kCFAllocatorDefault, Int(self.size.width), Int(self.size.height), kCVPixelFormatType_32ARGB, attrs, &pixelBuffer)
+        guard (status == kCVReturnSuccess) else {
+            return nil
+        }
+        
+        CVPixelBufferLockBaseAddress(pixelBuffer!, CVPixelBufferLockFlags(rawValue: 0))
+        let pixelData = CVPixelBufferGetBaseAddress(pixelBuffer!)
+        
+        let rgbColorSpace = CGColorSpaceCreateDeviceRGB()
+        let context = CGContext(data: pixelData, width: Int(self.size.width), height: Int(self.size.height), bitsPerComponent: 8, bytesPerRow: CVPixelBufferGetBytesPerRow(pixelBuffer!), space: rgbColorSpace, bitmapInfo: CGImageAlphaInfo.noneSkipFirst.rawValue)
+        
+        context?.translateBy(x: 0, y: self.size.height)
+        context?.scaleBy(x: 1.0, y: -1.0)
+        
+        UIGraphicsPushContext(context!)
+        self.draw(in: CGRect(x: 0, y: 0, width: self.size.width, height: self.size.height))
+        UIGraphicsPopContext()
+        CVPixelBufferUnlockBaseAddress(pixelBuffer!, CVPixelBufferLockFlags(rawValue: 0))
+        
+        return pixelBuffer
+    }
+
 }
