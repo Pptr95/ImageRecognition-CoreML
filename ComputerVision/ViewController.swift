e447820c81f67aab409a4dbe6b3cb21d4904ce86
Petru Potrimba <petru.potrimba@studio.unibo.it>
2018-07-29 22:08:52 +0200

    add feature for browsing photo library

--- ComputerVision/ViewController.swift
+++ ComputerVision/ViewController.swift
@@ -8,17 +8,28 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
 
     @IBOutlet weak var textArea: UITextView!
     @IBOutlet weak var picture: UIImageView!
     @IBAction func selectPhoto(_ sender: UIButton) {
+        self.present(self.imagePicker, animated: true, completion: nil)
     }
+    
+    private let imagePicker = UIImagePickerController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.imagePicker.sourceType = .photoLibrary
+        self.imagePicker.delegate = self
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        dismiss(animated: true, completion: nil)
+        guard let pickedImage =  info[UIImagePickerController.InfoKey.originalImage] as? UIImage else {return}
+        self.picture.image = pickedImage
+    }
 
 }
 
